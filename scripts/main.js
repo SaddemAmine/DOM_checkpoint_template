ea0594d015d3f44ad6852db107cfea15c7cc5dbe
SaddemAmine <47611308+SaddemAmine@users.noreply.github.com>
2020-11-18 17:38:38 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,48 @@
-let articles = document.querySelectorAll('article')
+var articles = document.querySelectorAll('article')
 
-let sum = () => {
+function calSum(){
     let sum = 0;
-    document.querySelectorAll('.money').forEach(x => sum += parseInt(x.innerHTML))
-    document.querySelector('#price').innerHTML = sum
+    for(var i = 0; i < articles.length - 1; i++){
+        price = parseInt(articles[i].children[5].children[0].innerHTML)
+        sum += price
+    }
+
+    document.querySelector('#price').innerHTML = sum;
 }
 
-sum()
+calSum();
 
-for(let i = 0; i < articles.length; i++){
-    articles[i].children[2].addEventListener('change', ()=>{
-        let qnt = parseInt(articles[i].children[2].value)
-        articles[i].children[3].children[0].innerHTML = qnt * 100
-        sum()
+for(var i = 0; i < articles.length - 1; i++){
+    let qnt = articles[i].children[3];
+    let prx = articles[i].children[5].children[0];
+
+    articles[i].children[2].addEventListener('click', function(){
+        if(parseInt(qnt.innerHTML) > 0){
+            qnt.innerHTML = parseInt(qnt.innerHTML) - 1
+            prx.innerHTML = parseInt(qnt.innerHTML) * 100
+        }
+        calSum();
+    })
+    
+    articles[i].children[4].addEventListener('click', function(){
+        qnt.innerHTML = parseInt(qnt.innerHTML) + 1
+        prx.innerHTML = parseInt(qnt.innerHTML) * 100
+        calSum();
     })
 
-    articles[i].children[4].addEventListener('click', e =>{
-        e.target.parentElement.remove()
-        sum()
+    articles[i].children[6].addEventListener('click', function(e){
+        e.target.parentElement.remove();
+        articles = document.querySelectorAll('article')
+        calSum();
+        
+        if(articles.length == 1)
+            document.querySelector('h3').innerHTML = "Your shopping cart is empty!"
+    })
+
+    articles[i].children[7].addEventListener('click', function(e){
+        if(e.target.classList.contains('far'))
+            e.target.classList.replace('far','fas')
+        else
+            e.target.classList.replace('fas','far')
     })
 }
